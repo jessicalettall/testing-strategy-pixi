31c5478c50624fefa76cd84f581d2b6f35855dad
JLettall <jessica.lettall@skybettingandgaming.com>
2021-04-20 13:59:45 +0100

    set-up

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 import { Application, Graphics} from "pixi.js";
 import { Color } from "./helpers/Color";
+import gsap from "gsap";
 
 
 const app = new Application({
     backgroundColor: Color.SeaGreen,
-    width: 1000,
-    height: 700,
+    width: 500,
+    height: 350,
 });
 
 document.body.appendChild(app.view);
+
+const ball = new Graphics();
+ball.beginFill(Color.GhostWhite);
+ball.drawCircle(20, 20, 10);
+
+app.stage.addChild(ball);
+
+gsap.to(ball, {
+    x: 250,
+    y: 175,
+    delay: 1,
+    duration: 2
+});
+
+gsap.to(ball, {
+    x:450,
+    y:90,
+    duration: 4,
+    delay:5
+});
